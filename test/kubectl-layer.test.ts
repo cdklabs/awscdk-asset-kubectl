c6a0f7c3b9c2eaf9ef3c0008c4ff9857c21a719e
Madeline Kusters <80541297+madeline-k@users.noreply.github.com>
2022-10-21 08:57:12 -0700

    feat: include kubectl version explicitly in API (#44)
    Based on feedback from @rix0rrr, explicitly including the version number in the class name.
    
    This PR also bumps the major version to 2.0 so that it is clear to customers that this library is compatible with AWS CDK 2.0

--- test/kubectl-layer.test.ts
+++ test/kubectl-layer.test.ts
@@ -1,13 +1,13 @@
 import { Stack } from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
-import { KubectlLayer } from '../lib';
+import { KubectlV23Layer } from '../lib';
 
 test('synthesized to a layer version', () => {
   // GIVEN
   const stack = new Stack();
 
   // WHEN
-  new KubectlLayer(stack, 'MyLayer');
+  new KubectlV23Layer(stack, 'MyLayer');
 
   // THEN
   Template.fromStack(stack).hasResourceProperties('AWS::Lambda::LayerVersion', {
