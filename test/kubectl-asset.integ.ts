8ae9c5fd6cde4919c71a2142035611e23a6d1808
Kaizen Conroy <zen.conroy@gmail.com>
2022-10-31 10:17:36 -0400

    feat: change `LAYER_SOURCE` to `LAYER_SOURCE_DIR`

--- test/kubectl-asset.integ.ts
+++ test/kubectl-asset.integ.ts
@@ -5,7 +5,6 @@ import * as s3_assets from 'aws-cdk-lib/aws-s3-assets';
 import * as cr from 'aws-cdk-lib/custom-resources';
 
 import { LAYER_SOURCE_DIR, ASSET_FILE } from '../lib';
-import { hashFile } from './util';
 
 /**
  * Test verifies that AWS CLI is invoked successfully inside Lambda runtime.
@@ -14,7 +13,7 @@ const app = new cdk.App();
 const stack = new cdk.Stack(app, 'lambda-layer-kubectl-integ-stack');
 const asset = new s3_assets.Asset(stack, 'layer-asset', {
   path: ASSET_FILE,
-  assetHash: hashFile(LAYER_SOURCE_DIR),
+  assetHash: cdk.FileSystem.fingerprint(LAYER_SOURCE_DIR),
 });
 
 const layer = new lambda.LayerVersion(stack, 'KubectlLayer', {
