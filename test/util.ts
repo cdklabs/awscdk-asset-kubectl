7146078919b7b39560b5fa30c599d81707d2a233
Kaizen Conroy <zen.conroy@gmail.com>
2022-10-31 10:18:51 -0400

    remove utils

--- test/util.ts
+++
@@ -1,9 +0,0 @@
-import * as crypto from 'crypto';
-import * as fs from 'fs';
-
-export function hashFile(fileName: string) {
-  return crypto
-    .createHash('sha256')
-    .update(fs.readFileSync(fileName))
-    .digest('hex');
-}
