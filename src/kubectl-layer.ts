90f340a044820f3a3ce253bc092d7a3c7c9d39a2
robertd <robert.djurasaj@gmail.com>
2023-04-13 14:04:45 -0600

    feat: add KubectLayer and release as @aws-cdk/lambda-layer-kubectl-v26

--- src/kubectl-layer.ts
+++ src/kubectl-layer.ts
@@ -5,13 +5,13 @@ import { assetHash, ASSET_FILE } from './_asset';
 /**
  * A CDK Asset construct that contains `kubectl` and `helm`.
  */
-export class KubectlV25Layer extends lambda.LayerVersion {
+export class KubectlV26Layer extends lambda.LayerVersion {
   constructor(scope: Construct, id: string) {
     super(scope, id, {
       code: lambda.Code.fromAsset(ASSET_FILE, {
         assetHash: assetHash(),
       }),
-      description: '/opt/kubectl/kubectl 1.25; /opt/helm/helm 3.11',
+      description: '/opt/kubectl/kubectl 1.26; /opt/helm/helm 3.11',
       license: 'Apache-2.0',
     });
   }
