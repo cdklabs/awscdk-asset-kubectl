36ee30a19f35b2614fda36694355cf081dbb51c8
GZ <hz351086153@gmail.com>
2025-02-18 14:12:33 -0800

    fix: update kubectl v32 helm version (#1667)
    Update kubectl v33 helm version to 3.17.1

--- test/kubectl-layer.test.ts
+++ test/kubectl-layer.test.ts
@@ -11,7 +11,7 @@ test('synthesized to a layer version', () => {
 
   // THEN
   Template.fromStack(stack).hasResourceProperties('AWS::Lambda::LayerVersion', {
-    Description: '/opt/kubectl/kubectl 1.32.0; /opt/helm/helm 3.17.0',
+    Description: '/opt/kubectl/kubectl 1.32.0; /opt/helm/helm 3.17.1',
   });
 });
 
