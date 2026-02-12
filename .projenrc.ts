08097f1ebe0ab8717ae6703c2164c04226184509
Ahmed Hamed <aemada@amazon.com>
2026-02-12 14:19:54 +0000

    feat: enable npm provenance for trusted publisher

--- .projenrc.ts
+++ .projenrc.ts
@@ -47,6 +47,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   },
   majorVersion: 2,
   npmAccess: NpmAccess.PUBLIC,
+  npmProvenance: true,
   releaseTagPrefix: `kubectl-v${SPEC_VERSION}`,
   releaseWorkflowName: releaseWorkflowName,
   // If we don't do this we release the devDependency updates that happen every day, which blows out
