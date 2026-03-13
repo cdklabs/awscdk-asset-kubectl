927f6e393694117e0369fb78bc31c97a4790ad20
gasolima <141347041+gasolima@users.noreply.github.com>
2026-03-13 13:39:38 +0100

    Update kubectl version in kubectl-layer.ts

--- src/kubectl-layer.ts
+++ src/kubectl-layer.ts
@@ -11,7 +11,7 @@ export class KubectlV35Layer extends lambda.LayerVersion {
       code: lambda.Code.fromAsset(ASSET_FILE, {
         assetHash: assetHash(),
       }),
-      description: '/opt/kubectl/kubectl 1.35.0; /opt/helm/helm 4.1.3',
+      description: '/opt/kubectl/kubectl 1.35.2; /opt/helm/helm 4.1.3',
       license: 'Apache-2.0',
     });
   }
