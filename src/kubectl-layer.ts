f07e6eb2d070954b9c9ab5423fd832c1ae576d0b
Zach Jacobson <72418868+zjaco13@users.noreply.github.com>
2026-07-15 09:54:32 -0700

    feat: update to kubectl 1.36 and helm 4.2 (#2772)
    Need new branch: kubectl-v36/main

--- src/kubectl-layer.ts
+++ src/kubectl-layer.ts
@@ -5,13 +5,13 @@ import { assetHash, ASSET_FILE } from './_asset';
 /**
  * A CDK Asset construct that contains `kubectl` and `helm`.
  */
-export class KubectlV35Layer extends lambda.LayerVersion {
+export class KubectlV36Layer extends lambda.LayerVersion {
   constructor(scope: Construct, id: string) {
     super(scope, id, {
       code: lambda.Code.fromAsset(ASSET_FILE, {
         assetHash: assetHash(),
       }),
-      description: '/opt/kubectl/kubectl 1.35.2; /opt/helm/helm 4.1.3',
+      description: '/opt/kubectl/kubectl 1.36.2; /opt/helm/helm 4.2.2',
       license: 'Apache-2.0',
     });
   }
